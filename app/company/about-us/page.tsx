3938e02195976a28ab538d6cd318469f86e983f3
Jai-Irkal <jai.vinodirkal@emcus.co.in>
2026-08-05 15:32:08 +0530

    About Us Page Completed

--- app/company/about-us/page.tsx
+++ app/company/about-us/page.tsx
@@ -1,9 +1,11 @@
 import HeaderComponent from "@/src/components/HeaderComponent/HeaderComponent";
-import HomeBanner from "@/public/banners/home-banner.jpeg"
 import Image from "next/image";
 import FooterComponent from "@/src/components/FooterComponent/FooterComponent";
-import Vignesh from "@/public/about-us/vignesh.png";
-import LinkedinIcon from "@/public/footer-assets/linkedin.svg";
+import GlobalPresence from "@/public/about-us/GlobalPresence.svg"
+import OurMission from "@/public/about-us/OurMission.svg";
+import OurVision from "@/public/about-us/OurVision.svg";
+import { WHAT_MAKES_EMCUS_DIFFERENT } from "@/src/data/emcus-different.data";
+import DifferenceCard from "@/src/components/cards/DifferenceCard";
 
 export default function AboutUs() {
   return (
@@ -12,7 +14,7 @@ export default function AboutUs() {
         <HeaderComponent active="ABOUT US" />
         <div className="relative w-full h-[300px] lg:h-[600px] 2xl:h-[600px] 2xl:bottom-0">
           <Image
-            src={HomeBanner}
+            src={GlobalPresence}
             alt="Home Banner"
             fill
             priority
@@ -24,46 +26,59 @@ export default function AboutUs() {
           {/* Overlay Content */}
           <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-6 z-10">
             <h1 className="text-white text-xl xl:text-4xl lg:text-4xl lg:font-medium font-normal xl:font-bold xl:px-0 px-4 lg:-mt-20 lg:px-20">
-              <span className="font-medium">WE ARE</span><span className="font-bold"> EMCUS</span>
+              <span className="font-medium">Global Client Presence</span>
             </h1>
-            <span className="text-white xl:text-lg lg:text-[16px] lg:mt-6 lg:px-10 mt-4 w-200 xl:px-0 px-6">
-              At EMCUS, we act as your dedicated support, collaborating closely with your R&D teams, eliminating concerns about the next steps in the development process. Consider us your reliable extended team, always available to address questions and provide assistance.
-            </span>
-            <span className="font-bold text-white text-lg">
-              We love what we do and are excited to work with you!
-            </span>
           </div>
         </div>
-        <h1 className="text-[25px] text-[#d94536] font-bold text-center w-full mt-30">LEADERSHIP</h1>
-        <div className="w-full h-85 2xl:h-65 flex justify-center py-5 mb-30">
-          <div className="w-90 2xl:w-140 rounded-xl shadow-2xl grid grid-cols-3 gap-2">
-            <div className="col-span-1 p-4 rounded-lg">
-              <div className="mt-4 flex flex-col justify-center items-center h-20 w-20 rounded-full mt-8">
-                <Image
-                  src={Vignesh}
-                  alt="icon"
-                  className="w-full h-full object-cover"
-                />
-                <Image
-                  src={LinkedinIcon}
-                  alt="LinkedIn"
-                  className="w-8 h-8 object-contain relative top-4"
-                />
+        <h1 className="text-[25px] text-[#d94536] font-bold text-left w-full mt-8 px-10">ABOUT EMCUS</h1>
+        <div className="w-full flex justify-center py-5">
+          <span className="px-10 text-justify text-[18px] text-[#64748B]">
+            EMCUS Technology Solutions is a specialized fire safety engineering partner for OEMs, delivering end-to-end design and development of fire & safety systems. With certification-aware engineering and expertise in UL and EN-54 readiness, we support customers throughout the product development and certification journey, accelerating the delivery of reliable, standards-ready fire safety products.
+          </span>
+        </div>
+        <div className="lg:px-10">
+          <div className="flex flex-col md:flex-row justify-center items-stretch gap-6">
+            <div className="w-full rounded-2xl border-[#E2E2E2] bg-[#D8E0F7] border-1 py-6 px-6 shadow-l">
+              <div className="flex items-center gap-4">
+                <div className="lg:w-[50px] lg:h-[48px] bg-[#243481] rounded-xl flex items-center justify-center">
+                  <Image
+                    src={OurVision}
+                    alt="Our Vision"
+                    className="w-10 h-10 object-contain"
+                  />
+                </div>
+                <h1 className="text-center text-[28px] font-bold text-[#444444]">Our Vision</h1>
+              </div>
+              <div className="mt-4 relative pr-12">
+                <p>To be the technology partner fire and safety companies trust most—known for engineering excellence, quality, compliance, and innovation.</p>
               </div>
             </div>
-
-            {/* 2/3 Column */}
-            <div className="relative flex flex-col col-span-2 p-4 rounded-lg 2xl:right-20">
-              <div className="flex lg:flex-col 2xl:flex-row 2xl:gap-1">
-                <span className="text-[24px] font-bold text-[#322986]">VIGNESH</span>
-                <span className="text-[24px] font-bold text-[#322986]">MOHAN</span>
+            <div className="w-full rounded-2xl border-[#E2E2E2] bg-[#FFE7E3] border-1 py-6 px-6 shadow-l">
+              <div className="flex items-center gap-4">
+                <div className="lg:w-[50px] lg:h-[48px] bg-[#E4312D] rounded-xl flex items-center justify-center">
+                  <Image
+                    src={OurMission}
+                    alt="Our Mission"
+                    className="w-10 h-10 object-contain"
+                  />
+                </div>
+                <h1 className="text-center text-[28px] font-bold text-[#444444]">Our Mission</h1>
               </div>
-              <span>Director</span>
-              <span className="mt-5">
-                Vignesh is an embedded systems professional with over 18 years experience in delivery of large and complex products and with 8+ years in leadership roles.
-              </span>
+              <div className="mt-4 relative pr-14">
+                <p>Helping fire and safety businesses build safer, smarter, and more connected products through world-class engineering.</p>
+              </div>
             </div>
           </div>
+        </div>
+        <h1 className="text-[25px] text-[#d94536] font-bold text-left w-full mt-8 px-10">WHAT MAKES EMCUS DIFFERENT</h1>
+        <div className="flex px-10 gap-5 flex-wrap py-6">
+          {
+            WHAT_MAKES_EMCUS_DIFFERENT.map((item, index) => {
+              return (
+                <DifferenceCard item={item} key={item.id}/>
+              )
+            })
+          }
         </div>
         <FooterComponent />
       </main>
---
+++ src/components/cards/DifferenceCard.tsx
@@ -0,0 +1,27 @@
+import { Difference } from '@/src/data/emcus-different.data'
+import React from 'react'
+import Image from 'next/image'
+
+const DifferenceCard = ({ item }: { item: Difference }) => {
+    return (
+        <div className="w-[250px] shadow-md p-4 rounded-xl">
+            <div className="flex items-center gap-3">
+                <div className="h-12 w-12 shrink-0 bg-[#FFE3E2] rounded-md border-2 border-[#E4312D] flex items-center justify-center">
+                    <Image
+                        src={item.image}
+                        alt={item.title}
+                        className="w-6 h-6"
+                    />
+                </div>
+
+                <p className="font-bold">{item.title}</p>
+            </div>
+
+            <p className="text-[#64748B] mt-2 text-[14px]">
+                {item.description}
+            </p>
+        </div>
+    )
+}
+
+export default DifferenceCard
