cf617871bac61c446912e444af10fe32b3bd3ba7
Jai-Irkal <jai.vinodirkal@emcus.co.in>
2026-08-19 11:06:55 +0530

    Corrected header for xl screens

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -68,7 +68,7 @@ const HeaderComponent = ({ active }: HeaderProps) => {
             {/* HEADER */}
             <div
                 className={`bg-white w-full fixed top-0 left-0 right-0 z-50 overflow-visible
-  flex items-center justify-between px-4 md:px-6 lg:px-10 shadow-sm
+  flex items-center justify-between px-4 md:px-6 lg:px-10 shadow-sm xl:px-88
   transition-all duration-300
   ${isScrolled && !isOpen ? "h-[60px] md:h-[70px]" : "h-[80px] md:h-[90px]"}`}
             >
